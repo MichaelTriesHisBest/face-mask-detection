8f80c432b99d7895c9fd4599bcb98e493db9a7d3
Brian Tonnies <brtonnies@gmail.com>
2020-10-08 16:16:54 -0500

    adding annotations 1801 -> 1901 to repository

--- data-to-repo.sh
+++ data-to-repo.sh
@@ -9,52 +9,52 @@ ANNOTATIONS=/Users/brtonnies/ArtificialIntelligence/semester-project/data/annota
 ANNOTATIONS_DEST=/Users/brtonnies/ArtificialIntelligence/face-mask-detection/data/annotations
 
 # do images, 100 at a time
-cd $IMAGES || null
-NUM_IMAGES=$(find $IMAGES -type f | wc -l)
-START=3570
-END=3670
-
-while [[ $END -lt $NUM_IMAGES ]]; do
-  echo "Start At File # $START"
-  echo "End At File # $END"
-  for n in {$START..$END}; do
-
-    cd $IMAGES || null
-    if [[ $n -lt 10 ]]; then
-      ZEROES="000"
-    elif [[ $n -lt 100 ]]; then
-      ZEROES="00"
-    elif [[ $n -lt 1000 ]]; then
-      ZEROES="0"
-    else
-      ZEROES=""
-    fi
-
-    f=$(find . -type f -name "$ZEROES$n\.*")
-    fpath="$IMAGES/${f:2}"
-    dest="$IMAGES_DEST/${f:2}"
-    if [[ -f "$fpath" ]]; then
-      cp "$fpath" "$dest"
-    fi
-  done
-
-  cd $PROJ_ROOT || null
-  git add .
-  git commit -am "adding images $START -> $END to repository"
-  git push -u origin main
-
-  if [[ ${END+100} -gt $NUM_IMAGES ]]; then
-    END=$NUM_IMAGES
-  else
-    END=$((END+100))
-  fi
-
-  if [[ ${START+100} -gt $NUM_IMAGES ]]; then
-    START=$NUM_IMAGES
-  else
-    START=$((START+100))
-  fi
-done
+#cd $IMAGES || null
+#NUM_IMAGES=$(find $IMAGES -type f | wc -l)
+#START=3570
+#END=3670
+#
+#while [[ $END -lt $NUM_IMAGES ]]; do
+#  echo "Start At File # $START"
+#  echo "End At File # $END"
+#  for n in {$START..$END}; do
+#
+#    cd $IMAGES || null
+#    if [[ $n -lt 10 ]]; then
+#      ZEROES="000"
+#    elif [[ $n -lt 100 ]]; then
+#      ZEROES="00"
+#    elif [[ $n -lt 1000 ]]; then
+#      ZEROES="0"
+#    else
+#      ZEROES=""
+#    fi
+#
+#    f=$(find . -type f -name "$ZEROES$n\.*")
+#    fpath="$IMAGES/${f:2}"
+#    dest="$IMAGES_DEST/${f:2}"
+#    if [[ -f "$fpath" ]]; then
+#      cp "$fpath" "$dest"
+#    fi
+#  done
+#
+#  cd $PROJ_ROOT || null
+#  git add .
+#  git commit -am "adding images $START -> $END to repository"
+#  git push -u origin main
+#
+#  if [[ ${END+100} -gt $NUM_IMAGES ]]; then
+#    END=$NUM_IMAGES
+#  else
+#    END=$((END+100))
+#  fi
+#
+#  if [[ ${START+100} -gt $NUM_IMAGES ]]; then
+#    START=$NUM_IMAGES
+#  else
+#    START=$((START+100))
+#  fi
+#done
 
 #cd $PROJ_ROOT || null
 cd $ANNOTATIONS || null
