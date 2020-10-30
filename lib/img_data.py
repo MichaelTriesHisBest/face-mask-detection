281af7961fa00d970389bad9d6edcb3829d83056
Brian Tonnies <brtonnies@gmail.com>
2020-10-30 16:05:43 -0500

    updating notebooks/model files

--- lib/img_data.py
+++ lib/img_data.py
@@ -84,7 +84,7 @@ def add_bboxes(df):
 
 
 def main():
-    detector = MTCNN()
+    mtcnn = MTCNN()
     images = get_images()
     annotations = get_annotations()
     images.sort()
@@ -94,42 +94,92 @@ def main():
     test_images = images[:1698]
 
     # process and write the training set
-    train_df = add_bboxes(train())
-    print(os.path.join(DATA_DIR, 'img_data', 'training.csv'))
-    train_df.to_csv(os.path.join(DATA_DIR, 'img_data', 'training.csv'))
+    # train_df = add_bboxes(train())
+    # print(os.path.join(DATA_DIR, 'img_data', 'training.csv'))
+    # train().to_csv(os.path.join(DATA_DIR, 'img_data', 'training.csv'))
 
-    # process and write the testing set for actual testing -- NOT TESTING HERE
-    test = list()
-    test_df = list()
+    # process and write the image data set to a file for easier access
+    data = list()
+    df = list()
+
     for image in test_images:
         img = plt.imread(os.path.join(IMAGES_DIR, image))
-        faces = detector.detect_faces(img)
+        print("Analyzing image '{}' (#{}/{})".format(
+            image,
+            test_images.index(image)+1,
+            len(test_images)
+        ))
 
-        t = list()
-        for face in faces:
-            bbox = face['box']
-            t.append([image, bbox])
-        test_df.append(t)
+        faces = mtcnn.detect_faces(img)
+        if not faces:
+            print("\tNo faces detected.")
+        else:
+            t = list()
+            for face in faces:
+                print("\tFinding bounds for {}/{} faces...".format(
+                    faces.index(face)+1,
+                    len(faces)
+                ))
+                bbox = face['box']
+                data.append([image, bbox])
+            df.append(t)
 
-    test += [i for i in test_df if len(i) > 1]
-    test += [[j for j in i] for i in test_df if len(i) == 1]
+    data += [i for i in df if len(i) == 1]
+    data += [[j for j in i] for i in df if len(i) > 1]
 
-    sub = [i[0] for i in test]
-    remaining_images = [image for image in test_images if image not in sub]
+    # neg = list()
+    # for i in data:
+    #     for j in i[1]:
+    #         if j < 0:
+    #             neg.append(i)
 
-    rem_df = list()
-    for image in remaining_images:
-        img = plt.imread(os.path.join(IMAGES_DIR, image))
-        faces = detector.detect_faces(img)
+    neg = [[j for j in i[1]] for i in data]
 
-        t = list()
-        for face in faces:
-            bbox = face['box']
-            t.append([image, bbox])
-        rem_df.append(t)
+    # for image in test_images:
+    #     img = plt.imread(os.path.join(IMAGES_DIR, image))
+    #     faces = mtcnn.detect_faces(img)
+    #
+    #     t = list()
+    #     for face in faces:
+    #         bbox = face['box']
+    #         t.append([image, bbox])
+    #     test_df.append(t)
+    #
+    # test += [i for i in test_df if len(i) > 1]
+    # test += [[j for j in i] for i in test_df if len(i) == 1]
+    #
+    # sub = [i[0] for i in test]
+    # remaining_images = [image for image in test_images if image not in sub]
+    #
+    # rem_df = list()
+    # for image in remaining_images:
+    #     img = plt.imread(os.path.join(IMAGES_DIR, image))
+    #     faces = mtcnn.detect_faces(img)
+    #
+    #     t = list()
+    #     for face in faces:
+    #         bbox = face['box']
+    #         t.append([image, bbox])
+    #     rem_df.append(t)
+    #
+    #     test += [i for i in rem_df if len(i) > 1]
+    #     test += [[j for j in i] for i in rem_df if len(i) == 1]
 
-        test += [i for i in rem_df if len(i) > 1]
-        test += [[j for j in i] for i in rem_df if len(i) == 1]
+    # print(data)
+
+    dframe = pd.DataFrame(
+        # data=data,
+        # columns=['name', 'x1', 'x2', 'y1', 'y2', 'classname']
+    )
+    dframe['name'] = [i[0] for i in data]
+    dframe['x1'] = [i[1][0] for i in data]
+    dframe['x2'] = [i[1][1] for i in data]
+    dframe['y1'] = [i[1][2] for i in data]
+    dframe['y2'] = [i[1][3] for i in data]
+    dframe['classname'] = [None for i in data]
+    print(dframe)
+    dframe.to_csv(os.path.join(DATA_DIR, 'img_data', 'not_classified.csv'))
+
 
 main()
 
